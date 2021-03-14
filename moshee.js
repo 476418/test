8b2b3373424a7985c0aca144a196a8286d6122bf
moshe <73557338+476418@users.noreply.github.com>
2021-03-14 19:49:23 +0200

    ihui js

---
+++ moshee.js
@@ -0,0 +1,12 @@
+console.log("hello mosh 1");
+console.log("hello mosh 2");
+
+for (let mosh = 1; mosh <=10; mosh++) {
+    console.log(mosh);
+}
+console.log(1+5 * 11-3); // לפי סדר פעולות מתמטיקה הוא חישב
+    console.log("mosh + good");
+
+    console.log("plize");
+
+    
