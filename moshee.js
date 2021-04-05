9c95b82544fc10aef7f2b44041bdfa66347c7f3d
moshe <73557338+476418@users.noreply.github.com>
2021-04-05 17:14:59 +0300

    h1 comite

--- moshee.js
+++ moshee.js
@@ -4,9 +4,22 @@ console.log("hello mosh 2");
 for (let mosh = 1; mosh <=10; mosh++) {
     console.log(mosh);
 }
+
+/*for ( mos = 1; mos <=10; mos++) {
+    console.log(mos);
+}*/
+
 console.log(1+5 * 11-3); // לפי סדר פעולות מתמטיקה הוא חישב
     console.log("mosh + good");
 
-    console.log("plize");
+    console.log(document);
 
-    
+  console.log(document.getElementById("vor"));
+  document.getElementById("vor").innerHTML = console.log ; //שינוי הדף html"
+
+  console.log(document.getElementById("mo"));
+  document.getElementById("mo").innerHTML = 'good';
+  console.log(1+5 * 11-6);
+ 
+  console.log(document.getElementById("agg"));
+  document.getElementById("agg").innerHTML = 'very good';
